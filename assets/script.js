f2c3027932d32f9b6f8bd1b22aa38ae292154253
Edward Wells <edward.wells87@gmail.com>
2023-04-23 10:32:51 -0500

    function populates titles, symbols, his los

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,10 @@
-//make const items that wont change
-const timeCounter = document.getElementById('timee')
 
-//need an array to store weather history data. use push or similar function to keep adding to and not changing the data itself
-//api data for the lat and long
+const timeCounter = document.getElementById('timee')
+let resultsDisplay = document.getElementById('displayCont')
+var cityElement = document.getElementById('cityName');
+let replacement_symbol = "_";
+let cityHistory = [];
+let cityTitleEl = document.getElementById('cityTitle')
 const options = {
   method: 'GET',
   headers: {
@@ -10,16 +12,13 @@ const options = {
     'X-RapidAPI-Host': 'forward-reverse-geocoding.p.rapidapi.com'
   }
 };
+//lets clean up the click function
 
-//make a mega array for the history, not just an array for this one.
-//function that sends city name into lat and long through other api
-let replacement_symbol = "_";
-let cityHistory = [];
+
 
 function theClick() {
-  var inputElement = document.getElementById('cityName');
-  elemVal = inputElement.value;
-  console.log(elemVal);
+  event.preventDefault();
+  elemVal = cityElement.value;
   stringElem = elemVal.toString();
   replacement_symbol = "%20";
   fixedInput = stringElem.replace(" ", replacement_symbol);
@@ -30,12 +29,13 @@ function theClick() {
   fetch(latLongUrlArry, options)
     .then(response => response.json())
     .then(response => {
-      console.log(response[0].lat);
-      console.log(response[0].lon);
       const city = elemVal;
+      console.log(elemVal)
+      let capElemVal = elemVal.toUpperCase();
+      
+      cityTitleEl.innerText = capElemVal;
       const latitude = response[0].lat;
       const longitude = response[0].lon;
-
       const cityData = { city, latitude, longitude };
       cityHistory.push(cityData);
       localStorage.setItem('cityHistory', JSON.stringify(cityHistory));
@@ -44,74 +44,88 @@ function theClick() {
       const weatherUrl = `http://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&units=imperial&appid=${weatherApi}`;
       console.log(weatherUrl);
       fetch(weatherUrl)
-  .then(response => response.json())
-  .then(myWeatherData => {
-    localStorage.setItem("myWeatherData", JSON.stringify(myWeatherData)); // Update this line
-    console.log(myWeatherData.list[0].main.temp);//path to current temp 
-    console.log(myWeatherData.list[1].main.temp_min)//tomorrow's minimum
-    console.log(myWeatherData)
-  })
-  .catch(error => console.error(error));
+        .then(response => response.json())
+        .then(myWeatherData => {
+          localStorage.setItem("myWeatherData", JSON.stringify(myWeatherData)); // Update this line
+        
+          console.log(myWeatherData)
+          pullWeatherData()
+         
+        })
+        .catch(error => console.error(error));
 
-      pullWeatherData();
+      
     })
     .catch(err => console.error(err));
+  
 }
 
-//function that calls data to display on the page
+let temp1 = document.getElementById('todaysTemp')
+let todhumel = document.getElementById('humidity')
+let todpressel = document.getElementById('pressure')
+let templowtom = document.getElementById('lowtom')
 
+let tomHiEl = document.getElementById('hitom')
+let thirdHiEl = document.getElementById('turdhi')
+let thirdLowEl = document.getElementById('turdlo')
+let frthhiEl = document.getElementById('fourthhi')
+let frthloEl = document.getElementById('fourthlo')
+let fifthhighEl = document.getElementById('fifthi')
+let fifthlowEl = document.getElementById('fifthlo')
 
-let pressure = document.getElementById('pressure')
-let humidity = document.getElementById('humidity')
-let sunrise = document.getElementById('sunrise')
-let sunset = document.getElementById('sunset')
-
-
-// pull data from weather and use it to fill in these elements
-// Assuming the weather data array is stored in a variable called 'cityHistoryPulled'
-// and the elements in the webpage are accessed using the variables pressure, humidity, sunrise, and sunset
+//img elems
+todImgel = document.getElementById('todImg')
+tomImgel = document.getElementById('tomImg')
+turdImgel = document.getElementById('turdImg')
+frthImgEl = document.getElementById('frthImg')
+fifthImgEl = document.getElementById('fifthImg')
 
 function pullWeatherData() {
   var cityWeatherPulled = JSON.parse(localStorage.getItem("myWeatherData"));
   console.log(cityWeatherPulled)
   
-  
-  
+    let todayTemp = cityWeatherPulled.list[0].main.temp;
+temp1.innerHTML = todayTemp + ' °F';
+    let todaySymbl = cityWeatherPulled.list[0].weather[0].icon;
+    console.log(todaySymbl)
+    todImg.src = `https://openweathermap.org/img/wn/${todaySymbl}@2x.png`
+    let todaysHumid = cityWeatherPulled.list[0].main.humidity;
+    todhumel.innerText = 'Humidity  ' + todaysHumid;
+    let todaysPres = cityWeatherPulled.list[0].main.pressure;
+todpressel.innerText = 'Pressure  ' + todaysPres;
 
-  if (cityWeatherPulled && cityWeatherPulled.list) {
-  // Extract the relevant weather information from the weather data
-  
+    let tomLow = cityWeatherPulled.list[3].main.temp_min;
+  templowtom.innerText = 'Low ' + tomLow + ' °F'
+    let tomHi = cityWeatherPulled.list[3].main.temp_max;
+    tomHiEl.innerText = 'High ' + tomHi + ' °F'
+    let tomSym = cityWeatherPulled.list[3].weather[0].icon;
+tomImgel.src = `https://openweathermap.org/img/wn/${tomSym}@2x.png`
+
+    let thirdLow = cityWeatherPulled.list[11].main.temp_min;
+    thirdLowEl.innerText = 'Low ' + tomLow + ' °F'
+    let thirdHi = cityWeatherPulled.list[11].main.temp_max
+    thirdHiEl.innerText = 'High ' + thirdHi + ' °F'
+    let thirdSymbl = cityWeatherPulled.list[11].weather[0].icon
+    turdImgel.src = `https://openweathermap.org/img/wn/${thirdSymbl}@2x.png`
 
 
+    let frthlw = cityWeatherPulled.list[19].main.temp_min
+    frthloEl.innerText = 'Low ' + frthlw + ' °F'
+    let frthhi = cityWeatherPulled.list[19].main.temp_max
+    frthhiEl.innerText = 'High ' + frthhi + ' °F'
+    let frthsym = cityWeatherPulled.list[19].weather[0].icon
+    frthImgEl.src = `https://openweathermap.org/img/wn/${frthsym}@2x.png`
 
 
-  // Update the elements in the webpage with the extracted weather information
-  
+    let fifthlow = cityWeatherPulled.list[27].main.temp_min
+    fifthlowEl.innerText = 'Low ' + fifthlow + ' °F'
+    let fifthhigh = cityWeatherPulled.list[27].main.temp_max
+    fifthhighEl.innerText = 'High ' + fifthhigh + ' °F'
+    let fifthsymb = cityWeatherPulled.list[27].weather[0].icon
+    fifthImgEl.src = `https://openweathermap.org/img/wn/${fifthsymb}@2x.png`
 
- 
-/// need to clean an refactor code so functions are smaller 
 
-function populateWeatherData () {
-  //declare all the variables now 
-let todayTemp =  myWeatherData.list[0].main.temp ; 
-let todaySymbl = myWeatherData.list[0].weather.icon; 
-let todaysHumid = myWeatherData.list[0].main.humidity;
-let todaysPres = myWeatherData.list[0].main.pressure;
-let tomLow = myWeatherData.list[3].main.temp_min;   
-let tomHi = myWeatherData.list[3].main.temp_max ;  
-let tomSym = myWeatherData.list[3].weather.icon;
-let thirdLow = myWeatherData.list[11].main.temp_min ;  
-let thirdHi = myWeatherData.list[11].main.temp_max
-let thirdSymbl = myWeatherData.list[11].weather.icon
-let frthlw = myWeatherData.list[19].main.temp_min
-let frthhi = myWeatherData.list[19].main.temp_max
-let frthsym = myWeatherData.list[19].weather.icon
-let fifthlow = myWeatherData.list[27].main.temp_min
-let fifthhigh = myWeatherData.list[27].main.temp_max
-let fifthsymb = myWeatherData.list[27].weather.icon
+   
+  }
 
-/// populate data 
-document.getElementById("pressure").textContent = "Pressure: " + pressureValue + " hPa";
-document.getElementById("humidity").textContent = "Humidity: " + humidityValue + "%";
-document.getElementById('').textcontent = ' ';
-}
+  //figure out symbols on page 
